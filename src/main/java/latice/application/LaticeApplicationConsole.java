0422c35741f6fde9c63943968ebc7db149cf81e3
Henzo RENET <henzodu33@gmail.com>
2025-04-01 15:16:48 +0200

    ajout LaticeApplicationConsole

---
+++ src/main/java/latice/application/LaticeApplicationConsole.java
@@ -0,0 +1,12 @@
+package latice.application;
+
+public class LaticeApplicationConsole {
+	private static final String LINE_BAR = "-----------------------------------------------------";
+	
+	public static void main(String[] args) {
+		System.out.println(LINE_BAR);
+		System.out.println("-- Bienvenue dans notre magnifique jeu de latice ! --");
+		System.out.println(LINE_BAR);
+	}
+
+}
