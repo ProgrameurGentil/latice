ba4653001f91efcaef4d8598f13a3d64e16556a7
Henzo RENET <henzodu33@gmail.com>
2025-04-17 09:03:45 +0200

    ajout des classes GoupeDeTuile et Joueur

---
+++ src/main/java/latice/model/GroupeDeTuile.java
@@ -0,0 +1,18 @@
+package latice.model;
+
+import java.util.ArrayList;
+
+public abstract class GroupeDeTuile {
+	private ArrayList<Tuile> tuiles;
+
+	public GroupeDeTuile(ArrayList<Tuile> tuiles) {
+		this.tuiles = tuiles;
+	}
+
+	public Integer getTaille() {
+		//TODO ajouter le code pour avoir la taille
+		return 0;
+	}
+	
+	
+}
---
+++ src/main/java/latice/model/Joueur.java
@@ -0,0 +1,43 @@
+package latice.model;
+
+public class Joueur {
+	private final String nom;
+	private Integer points;
+	private Integer actionsRestante;
+	private Integer nbTuilesPosees;
+	
+	public Joueur(String nom, Integer points, Integer actionsRestante, Integer nbTuilesPosees) {
+		this.nom = nom;
+		this.points = points;
+		this.actionsRestante = actionsRestante;
+		this.nbTuilesPosees = nbTuilesPosees;
+	}
+	
+	public Joueur(String nom) {
+		this(nom, 0, 0, 0);
+	}
+	
+	public void piocher(Rack rack, Pioche pioche) {
+		//TODO
+	}
+	
+	public void echangerRack(Rack rack, Pioche pioche) {
+		//TODO
+	}
+	
+	public Integer jouer(Rack rack, Tuile tuile, Case case) {
+		//TODO
+	}
+	
+	public Integer acheterAction() {
+		//TODO
+	}
+	
+	public void passerAction() {
+		//TODO
+	}
+	
+	public Integer finAction() {
+		//TODO
+	}
+}
