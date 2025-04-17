dbfadccc357a83e41212c55160ed109a389b5d93
Henzo RENET <henzodu33@gmail.com>
2025-04-17 08:33:32 +0200

    ajout de la classe Tuile

---
+++ src/main/java/latice/application/Tuile.java
@@ -0,0 +1,11 @@
+package latice.application;
+
+public class Tuile {
+	private final Couleur couleur;
+	private final Forme forme;
+	
+	public Tuile(Couleur couleur, Forme forme) {
+		this.couleur = couleur;
+		this.forme = forme;
+	}
+}
