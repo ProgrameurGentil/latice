7738353467c805ad4b44a330abbdd4979d1202f5
Evan Arranga <evanarranga905@gmail.com>
2025-04-17 09:48:29 +0200

    Ajout de maitre du jeu

---
+++ src/main/java/latice/model/MaitreDuJeu.java
@@ -0,0 +1,19 @@
+package latice.model;
+
+public class MaitreDuJeu {
+	private Integer nb_tours;
+	
+	public Boolean gagner(Joueur joueur) {
+		//TODO
+		return null;
+	}
+	public void lancerTour() {
+		//TODO
+	}
+	public Joueur aQuiLeTour() {
+		//TODO
+		return null;
+		
+	}
+}
+
