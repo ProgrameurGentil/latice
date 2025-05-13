ea5dd751a8edd06218b015b7f8e4333f03a6f487
Henzo RENET <henzodu33@gmail.com>
2025-05-13 12:02:27 +0200

    ajout de teste sur les tuiles

--- src/test/java/latice/TestGestionDesTuiles.java
+++ src/test/java/latice/TestGestionDesTuiles.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.*;
 
@@ -12,42 +13,41 @@ import latice.model.*;
 
 public class TestGestionDesTuiles {
 	MaitreDuJeu maitre = new MaitreDuJeu();
-	GroupeDeTuile listeDeTuiles = new GroupeDeTuile(new ArrayList<Tuile>());
+	ArrayList<Tuile> listeDeTuiles = new ArrayList<Tuile>();
 	ArrayList<Joueur> listeJoueur = new ArrayList<Joueur>();
 	
 	@BeforeEach
 	public void ajouterDesElementDansLaListePourFaireLesTests() {
-		listeDeTuiles.ajouter(new Tuile(Couleur.BLEU, Forme.DAUPHIN));
-		listeDeTuiles.ajouter(new Tuile(Couleur.BLEU, Forme.FLEUR));
-		listeDeTuiles.ajouter(new Tuile(Couleur.ROUGE, Forme.DAUPHIN));
-		listeDeTuiles.ajouter(new Tuile(Couleur.ROUGE, Forme.FLEUR));
-		listeDeTuiles.ajouter(new Tuile(Couleur.VERT, Forme.DAUPHIN));
-		listeDeTuiles.ajouter(new Tuile(Couleur.VERT, Forme.FLEUR));
-		listeDeTuiles.ajouter(new Tuile(Couleur.GRIS, Forme.DAUPHIN));
-		listeDeTuiles.ajouter(new Tuile(Couleur.GRIS, Forme.FLEUR));
-		listeDeTuiles.ajouter(new Tuile(Couleur.JAUNE, Forme.DAUPHIN));
-		listeDeTuiles.ajouter(new Tuile(Couleur.JAUNE, Forme.FLEUR));
+		listeDeTuiles.add(new Tuile(Couleur.BLEU, Forme.DAUPHIN));
+		listeDeTuiles.add(new Tuile(Couleur.BLEU, Forme.FLEUR));
+		listeDeTuiles.add(new Tuile(Couleur.ROUGE, Forme.DAUPHIN));
+		listeDeTuiles.add(new Tuile(Couleur.ROUGE, Forme.FLEUR));
+		listeDeTuiles.add(new Tuile(Couleur.VERT, Forme.DAUPHIN));
+		listeDeTuiles.add(new Tuile(Couleur.VERT, Forme.FLEUR));
+		listeDeTuiles.add(new Tuile(Couleur.GRIS, Forme.DAUPHIN));
+		listeDeTuiles.add(new Tuile(Couleur.GRIS, Forme.FLEUR));
+		listeDeTuiles.add(new Tuile(Couleur.JAUNE, Forme.DAUPHIN));
+		listeDeTuiles.add(new Tuile(Couleur.JAUNE, Forme.FLEUR));
+		
+		listeJoueur.add(new Joueur("J1"));
+		listeJoueur.add(new Joueur("J2"));
 	}
 	@Test
 	public void diviserUneListeDeTuillesEnDeuxEtAvoirUneSeparationJuste() {
-		GroupeDeTuile listeDeTuiles1 = new GroupeDeTuile(new ArrayList<Tuile>());
+		maitre.diviserTuilesEnDeux(listeDeTuiles, listeJoueur);
 		
-		GroupeDeTuile listeDeTuilesDeVerif = new GroupeDeTuile(new ArrayList<Tuile>());
-		listeDeTuilesDeVerif.ajouter(new Tuile(Couleur.BLEU, Forme.DAUPHIN));
-		listeDeTuilesDeVerif.ajouter(new Tuile(Couleur.BLEU, Forme.FLEUR));
-		listeDeTuilesDeVerif.ajouter(new Tuile(Couleur.ROUGE, Forme.DAUPHIN));
-		listeDeTuilesDeVerif.ajouter(new Tuile(Couleur.ROUGE, Forme.FLEUR));
-		listeDeTuilesDeVerif.ajouter(new Tuile(Couleur.VERT, Forme.DAUPHIN));
-		
-		maitre.diviserTuilesEnDeux(listeDeTuiles, listeDeTuiles1, new ArrayList<Tuile>());
-		System.out.println(listeDeTuiles1);
-		System.out.println(listeDeTuilesDeVerif);
-		
-		assertEquals(listeDeTuiles1.obtenirValeur(0), listeDeTuilesDeVerif.obtenirValeur(0));
-		assertEquals(listeDeTuiles1.obtenirValeur(1), listeDeTuilesDeVerif.obtenirValeur(1));
-		assertEquals(listeDeTuiles1.obtenirValeur(2), listeDeTuilesDeVerif.obtenirValeur(2));
-		assertEquals(listeDeTuiles1.obtenirValeur(3), listeDeTuilesDeVerif.obtenirValeur(3));
-		assertEquals(listeDeTuiles1.obtenirValeur(4), listeDeTuilesDeVerif.obtenirValeur(4));
+		assertEquals(listeJoueur.get(0).pioche.taille(), listeJoueur.get(1).pioche.taille());
+	}
+	
+	@Test
+	public void initialisationsDesTuilesEtVerificationDeLaTaille() {
+		List<Tuile> listeDeTouteLesTuiles = maitre.initTuiles();
+		assertEquals(listeDeTouteLesTuiles.size(), 72);
+	}
+	
+	@Test
+	public void melangerUnGoupeDeTuile() {
+		//TODO
 	}
 	
 	/*
