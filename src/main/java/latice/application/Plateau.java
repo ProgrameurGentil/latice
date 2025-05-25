9214c5ef9c7f919a9308f3215bc516d284c12be4
Evan Arranga <evanarranga905@gmail.com>
2025-05-25 22:20:08 +0200

    création d'une pop-up pour le menu des actions, d'une fonction pour créer un bouton dans le menu

--- src/main/java/latice/application/Plateau.java
+++ src/main/java/latice/application/Plateau.java
@@ -3,6 +3,7 @@ package latice.application;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -19,7 +20,12 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 import latice.model.Joueur;
 import latice.model.PlateauDeCase;
 import latice.model.PlateauTuiles;
@@ -30,9 +36,10 @@ public class Plateau extends Application{
 	 private static final int colonnes = 9;
 	 private static final int lignes = 9;
 	 private static final double case_taille = 62.0;
+	 private static final int btn_taille = 175;
 	 private HBox hbRack = new HBox();
 	 
-	 private static Plateau instance; // <-- Référence statique
+	 private static Plateau instance; 
 	 public static Plateau getInstance() { return instance; }
 	 
 	 
@@ -64,8 +71,10 @@ public class Plateau extends Application{
 
 			hbRack.setBackground(new Background(backgroundImage)); 
 			
-			PlateauTuiles plateauTuile = new PlateauTuiles(); // temporaire
+
+			//PlateauTuiles plateauTuile = new PlateauTuiles(); // temporaire
 			PlateauDeCase plateauCase = new PlateauDeCase();
+
 			
 			GridPane plateau = new GridPane();
 	        plateau.setGridLinesVisible(false);
@@ -90,7 +99,7 @@ public class Plateau extends Application{
 	        for (int row = 0; row < lignes; row++) {
 	            for (int col = 0; col < colonnes; col++) {
 	                ImageView imageView = new ImageView();
-	                Dnd.cibleDragAndDrop(imageView, plateauTuile);
+	                Dnd.cibleDragAndDrop(imageView, null);
 	                imageView.setFitHeight(case_taille);
 	                imageView.setFitWidth(case_taille);
 	                imageView.setPreserveRatio(true);
@@ -102,6 +111,14 @@ public class Plateau extends Application{
 	                plateau.add(imageView, col, row);
 	            }
 	        }
+	        
+	        
+	        
+
+	        
+	        
+	        
+	        
 	        Image imgPlateau = new Image(getClass().getResourceAsStream("/plateau/plateau.png"));
 	        BackgroundImage background = new BackgroundImage(
 	        		imgPlateau,
@@ -115,13 +132,33 @@ public class Plateau extends Application{
 	        root.setCenter(plateau);
 	        
 
+
 	        VBox vbInformation = new VBox();
+
 	        
+	        VBox vb = new VBox();
+	        VBox vblbl = new VBox();
+	        VBox vboxbtn = new VBox();
 	        Label pointsJ1 = new Label("Points du joueur 1 : ");
 	        Label pointsJ2 = new Label("Points du joueur 2 : ");
+
+	        Button menuAction = new Button("Faire une Action");
+	        menuAction.setOnAction(e -> showMenuPopup());
+	        
+	        
+	        vblbl.setStyle("-fx-border-color: red;");
+	        pointsJ1.setTextFill(Color.RED);
+	        
+
+	        vboxbtn.getChildren().add(menuAction);
+	        vboxbtn.setStyle("-fx-border-color: green;");
+	        vblbl.getChildren().addAll(pointsJ1,pointsJ2);
+	        vb.getChildren().addAll(vboxbtn, vblbl);
+	        root.setLeft(vb);
+
 	        
 	        vbInformation.setStyle("-fx-border-color: red;");
-	        vbInformation.getChildren().addAll(pointsJ1,pointsJ2);
+	        vbInformation.getChildren().add(vb);
 	        
 	        root.setLeft(vbInformation);
 	        vbInformation.setAlignment(Pos.CENTER_LEFT);
@@ -154,6 +191,65 @@ public class Plateau extends Application{
 	            hbRack.getChildren().add(imageView);
 	        }
 		}
+	}
+	
+	private void showMenuPopup() {
+		
+        Stage menuStage = new Stage();
+        menuStage.initModality(Modality.APPLICATION_MODAL);
+        menuStage.setTitle("Menu");
+
+        Button btnpass = afficherboutton("passer son tour", "La joueur a passer son tour", menuStage); 
+        Button btnacheter = afficherboutton("Acheter une Action (2)", "Le joueur a choisi d'acheter", menuStage);
+        Button btnchangerRack = afficherboutton("Echanger son rack (2)", "Le joueur a échanger son rack", menuStage);
+        Button btnfermerAction = afficherboutton("retour", null, menuStage);
+                
+        VBox vbBoutton1et2 = new VBox(10, btnacheter, btnpass);
+        VBox vbBoutton3etfermer = new VBox(10, btnchangerRack, btnfermerAction);
+        
+        
+        
+
+        HBox menuaction = new HBox(15, vbBoutton1et2, vbBoutton3etfermer);
+        
+        
+        menuaction.setStyle("-fx-padding: 20; -fx-alignment: center;");
+        Scene menuScene = new Scene(menuaction, 400, 125);
+
+        menuStage.setScene(menuScene);
+        menuStage.showAndWait();
+        menuStage.setResizable(false);
+    }
+	
+	private Button afficherboutton(String string, String action, Stage stage) {
+		
+		final Button button = new Button(string);
+		
+		if (string != "retour") {
+			button.setOnAction(e -> System.out.println(action));
+		}else {
+			button.setOnAction(e -> stage.close());
+		}
+		
+		button.setMaxWidth(btn_taille);
+		
+		button.setStyle("-fx-background-radius: 15;" +
+        		"-fx-background-image: url('/Bouton/background_bouton.png');"+
+        	    "-fx-background-size: cover;" +
+        	    "-fx-background-size: 100% 100%;" +
+        	    "-fx-background-position: center;" +
+        	    "-fx-padding: 10 20;"+
+        	    "-fx-background-repeat: no-repeat;" +
+        	    "-fx-text-fill: lightblue;");
+        
+        button.layoutBoundsProperty().addListener((obs, ancVal, nouvVal) -> {
+            Rectangle clip = new Rectangle(nouvVal.getWidth(), nouvVal.getHeight());
+            clip.setArcWidth(20);
+            clip.setArcHeight(20);
+            button.setClip(clip);
+        });
+		return button;
+		
 	}
 }
 		
