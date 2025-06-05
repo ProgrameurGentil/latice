99985829521e68b4b1fa8fbfcff9224d89fa78e7
HOBON <adam.hobon@etu.unilim.fr>
2025-06-05 22:34:53 +0200

    debut implémentation des test ne concernant pas les points dans TestPoserTuile

--- src/test/java/latice/TestPoserTuile.java
+++ src/test/java/latice/TestPoserTuile.java
@@ -16,38 +16,97 @@ import latice.model.Rack;
 import latice.model.Tuile;
 
 public class TestPoserTuile {
-  MaitreDuJeu leMaitre = new MaitreDuJeu();
-  PlateauTuiles plateauTuiles = new PlateauTuiles();
-  Pioche touteLesTuile = new Pioche();
-  
-  @BeforeEach
+	MaitreDuJeu leMaitre = new MaitreDuJeu();
+	PlateauTuiles plateauTuiles = new PlateauTuiles();
+	Pioche touteLesTuile = new Pioche();
+	
+	@BeforeEach
 	public void initialisation() {
-  	PlateauDeCase plateauDeCase = PlateauDeCase.initialisationPlateauCase();
-  	touteLesTuile = Tuile.initialisationTuiles();
-    
-  	Tuile T1a = new Tuile(Couleur.ROUGE, Forme.OISEAU);
-  	Tuile T1b = new Tuile(Couleur.BLEU, Forme.OISEAU);
-  	Tuile T1c = new Tuile(Couleur.JAUNE, Forme.FLEUR);
-  	Tuile T1d = new Tuile(Couleur.VERT, Forme.DAUPHIN);
-  	Tuile T1e = new Tuile(Couleur.ROUGE, Forme.TORTUE);
-  	List<Tuile> lerack1 = new ArrayList<>(Arrays.asList(T1a, T1b, T1c, T1d, T1e));
-  	Rack rackUn = new Rack(lerack1);
-  		
-  	Tuile T2a = new Tuile(Couleur.BLEU, Forme.FLEUR);
-  	Tuile T2b = new Tuile(Couleur.JAUNE, Forme.PLUME);
-  	Tuile T2c = new Tuile(Couleur.JAUNE, Forme.PLUME);
-  	Tuile T2d = new Tuile(Couleur.ROUGE, Forme.DAUPHIN);
-  	Tuile T2e = new Tuile(Couleur.VERT, Forme.TORTUE);
-  	List<Tuile> lerack2 = new ArrayList<>(Arrays.asList(T2a, T2b, T2c, T2d, T2e));
-  	Rack rackDeux = new Rack(lerack2);
-  		
-    Joueur joueurTest1 = new Joueur("J1", rackUn, new Pioche() ) ;
-    Joueur joueurTest2 = new Joueur("J2", rackDeux, new ¨Pioche() ) ;
-    ArrayList<Joueur> listeJoueurs = new ArrayList<>(Arrays.asList(joueurTest1, joueurTest2)); 
-  
-    leMaitre.diviseEtRepartiLesTuilesEnPioches(touteLesTuile, listeJoueurs);
-  }
+		PlateauDeCase plateauDeCase = PlateauDeCase.initialisationPlateauCase();
+		touteLesTuile = Tuile.initialisationTuiles();
+		
+		Tuile Tuile1 = new Tuile(Couleur.ROUGE, Forme.OISEAU);
+		Tuile Tuile2 = new Tuile(Couleur.BLEU, Forme.OISEAU);
+		Tuile Tuile3 = new Tuile(Couleur.JAUNE, Forme.FLEUR);
+		Tuile Tuile4 = new Tuile(Couleur.VERT, Forme.DAUPHIN);
+		Tuile Tuile5 = new Tuile(Couleur.ROUGE, Forme.TORTUE);
+		List<Tuile> lerack = new ArrayList<>(Arrays.asList(Tuile1, Tuile2, Tuile3, Tuile4, Tuile5));
+		Rack rackJoueur = new Rack(lerack);
+			
+		Joueur joueurTest = new Joueur("J1", 0, 100, 0, rackJoueur, new Pioche() ) ; 
+	}
 
-  @test
-  
+	@test
+	public void testPoserTuileAuCentrePremierTour() {
+		assertFalse(joueurTest.poserTuile(3, new Position(3,4), plateauDeCase, plateauTuiles) );
+		assertTrue(joueurTest.poserTuile(3, new Position(4,4), plateauDeCase, plateauTuiles) );
+	}
+
+	@test
+	public void testPasPoserTuileSurTuile() {
+		joueurTest.poserTuile(3, new Position(4,4), plateauDeCase, plateauTuiles);
+		assertFalse(joueurTest.poserTuile(2, new Position(4,4), plateauDeCase, plateauTuiles) );
+	}
+
+	@test
+	public void testPasPoserTuileSeule() {
+		joueurTest.poserTuile(3, new Position(4,4), plateauDeCase, plateauTuiles);
+		assertFalse(joueurTest.poserTuile(2, new Position(2,8), plateauDeCase, plateauTuiles) );
+	}
+	
+	@test
+	public void testTailleRackAvantEtApresPoserTuile() {
+		assertEquals(5, joueurTest.getRack().taille());
+		joueurTest.poserTuile(3, new Position(4,4), plateauDeCase, plateauTuiles);
+		assertEquals(4, joueurTest.getRack().taille());
+	}
+	
+	@test
+	public void testPoserTuileQuandCorrespond() {
+		plateauTuiles.poser(new Position(4,4), new Tuile(Couleur.BLEU,Forme.DAUPHIN));
+		plateauTuiles.poser(new Position(3,3), new Tuile(Couleur.BLEU,Forme.FLEUR));
+		
+		assertTrue(joueurTest.poserTuile(2, new Position(3,4), plateauDeCase, plateauTuiles) );
+		assertTrue(joueurTest.poserTuile(4, new Position(4,3), plateauDeCase, plateauTuiles) );
+		assertFalse(joueurTest.poserTuile(3, new Position(4,3), plateauDeCase, plateauTuiles) );
+	}
+
+	@test
+	public void testNbTuilePoserAugment() {
+		assertEquals(0 , joueurTest.getNbTuilePose() );
+		joueurTest.poserTuile(3, new Position(3,4), plateauDeCase, plateauTuiles);
+		assertEquals(0 , joueurTest.getNbTuilePose() );
+		joueurTest.poserTuile(3, new Position(4,4), plateauDeCase, plateauTuiles);
+		assertEquals(1 , joueurTest.getNbTuilePose() );
+	}
+
+	@test
+	public void testGanerPointQuandDeuxAutour() {
+		//TODO
+	}
+
+	@test
+	public void testGanerPointQuandTroisAutour() {
+		//TODO
+	}
+
+	@test
+	public void testGanerPointQuandQuatreAutour() {
+		//TODO
+	}
+
+	@test
+	public void testGanerPointQuandDeuxAutourEtSurSoleil() {
+		//TODO
+	}
+
+	@test
+	public void testGanerPointQuandTroisAutourEtSurSoleil() {
+		//TODO
+	}
+
+	@test
+	public void testGanerPointQuandQuatreAutourEtSurSoleil() {
+		//TODO
+	}
 }
